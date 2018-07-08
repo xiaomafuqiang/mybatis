077b2a683befcab80861664f893d0be699091878
yy <mafuqiang@outlook.com>
2018-07-08 14:35:16 +0800

    interface mapper mybatis

---
+++ src/main/java/net/htmlonline/dao/EmployeeInterfaceMapper.java
@@ -0,0 +1,7 @@
+package net.htmlonline.dao;
+
+import net.htmlonline.bean.Employee;
+
+public interface EmployeeInterfaceMapper {
+    Employee getEmpById(Integer id);
+}
--- src/test/java/net/htmlonline/test/MybatisTest.java
+++ src/test/java/net/htmlonline/test/MybatisTest.java
@@ -1,6 +1,7 @@
 package net.htmlonline.test;
 
 import net.htmlonline.bean.Employee;
+import net.htmlonline.dao.EmployeeInterfaceMapper;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -12,11 +13,20 @@ import java.io.InputStream;
 
 public class MybatisTest {
 
+    private SqlSessionFactory getSessionFactory() throws IOException {
+
+        String resouce = "mybatis-conf.xml";
+        InputStream in = Resources.getResourceAsStream(resouce);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(in);
+        return sqlSessionFactory;
+    }
+
+    /*
+    * 普通写法， 老版本
+    * */
     @Test
     public void test() throws IOException {
-        String resource = "mybatis-conf.xml";
-        InputStream inputStream = Resources.getResourceAsStream(resource);
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSessionFactory sqlSessionFactory = getSessionFactory();
 
         SqlSession openSession = sqlSessionFactory.openSession();
         Employee employee = openSession.selectOne("EmployeeMapper.selectEmp", 1);
@@ -24,5 +34,19 @@ public class MybatisTest {
         System.out.println(employee);
         openSession.close();
 
+    }
+
+
+    /*
+    * 接口写法， 新版本
+    * */
+    @Test
+    public void testInterface() throws IOException {
+        SqlSessionFactory sqlSessionFactory = getSessionFactory();
+        SqlSession openSession = sqlSessionFactory.openSession();
+
+        EmployeeInterfaceMapper employeeInterfaceMapper = openSession.getMapper(EmployeeInterfaceMapper.class);
+        Employee employee = employeeInterfaceMapper.getEmpById(2);
+        System.out.println(employee);
     }
 }
